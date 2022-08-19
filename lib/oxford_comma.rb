ce744b59b85e30758229499fa9e564101bee3dc3
otieno-derrick <otieno.derrrick@student.moringaschool.com>
2022-08-20 01:04:11 +0300

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.count > 2 then
+        array[-1] = "and #{array.last}";
+        array.join(", ")
+    elsif array.count > 1 then
+        array.join(" and ")
+    else
+        array.join()
+    end
 end
+
